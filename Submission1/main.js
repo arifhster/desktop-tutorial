a5eb3509917d99f27cd015189de7cae1ca37bd79
arifhster <59981910+arifhster@users.noreply.github.com>
2020-08-22 02:15:36 +0700

    commit to me
    commit test

---
+++ Submission1/main.js
@@ -0,0 +1,40 @@
+function changeGambar1_1(){
+	document.getElementById("gambarFrog1").src="img/frog1.png";
+}
+function changeGambar1_2(){
+	document.getElementById("gambarFrog1").src="img/frog7.jpg";
+}
+
+function changeGambar2_1(){
+	document.getElementById("gambarFrog2").src="img/frog2.png";
+}
+function changeGambar2_2(){
+	document.getElementById("gambarFrog2").src="img/frog8.jpg";
+}
+function changeGambar3_1(){
+	document.getElementById("gambarFrog3").src="img/frog3.png";
+}
+function changeGambar3_2(){
+	document.getElementById("gambarFrog3").src="img/frog9.jpg";
+}
+
+function changeGambar4_1(){
+	document.getElementById("gambarFrog4").src="img/frog4.png";
+}
+function changeGambar4_2(){
+	document.getElementById("gambarFrog4").src="img/frog10.jpg";
+}
+
+function changeGambar5_1(){
+	document.getElementById("gambarFrog5").src="img/frog5.png";
+}
+function changeGambar5_2(){
+	document.getElementById("gambarFrog5").src="img/frog11.jpg";
+}
+
+function changeGambar6_1(){
+	document.getElementById("gambarFrog6").src="img/frog6.png";
+}
+function changeGambar6_2(){
+	document.getElementById("gambarFrog6").src="img/frog12.jpg";
+}
